7b926354aa78a53544fb8cd4a01d7e987542858e
GrayTurtle <john.tran@ou.edu>
2016-12-24 14:36:47 -0600

    remove whitespace

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,13 +15,10 @@ public class Main
 
     public static void main(String[] args)
     {
-
         String s = "https://www.amazon.com";//"https://en.wikipedia.org/wiki/Christmas_market";
         PageReader pageReader = new PageReader(s);
         //new UrlFinder().dumpPage(pageReader.getReader());
         new UrlFinder().getUrls(pageReader.getReader());
-
-
     }
 
 
